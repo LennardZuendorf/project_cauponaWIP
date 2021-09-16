87d72e1475ea426ef82d902df34422aa8eccf2ac
SGehrig97 <50809506+SGehrig97@users.noreply.github.com>
2021-09-16 17:47:23 +0200

    Delete initiate_mensalist_berlin.py

--- initiate_mensalist_berlin.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.526858%2C13.374758&radius=22000&keyword=mensa&key=AIzaSyDuLB1WqmL50sgYUS1wsfc2bBUlf0C_ffs"
-
-payload = {}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-print(response.text)
