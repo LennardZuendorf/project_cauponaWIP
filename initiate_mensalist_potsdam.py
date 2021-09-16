66c8f7f3ee574349ff177f2e416fd416d92e9637
SGehrig97 <50809506+SGehrig97@users.noreply.github.com>
2021-09-16 17:47:46 +0200

    Delete initiate_mensalist_potsdam.py

--- initiate_mensalist_potsdam.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.3906%2C13.0645&radius=11000&keyword=mensa&key=AIzaSyDuLB1WqmL50sgYUS1wsfc2bBUlf0C_ffs"
-
-payload = {}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-print(response.text)
