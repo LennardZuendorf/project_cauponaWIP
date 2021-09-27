a5e514696f4b427121c8711a169167bc9caab2b5
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-09-27 13:34:23 +0200

    c0.12 - setup views, components, router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Cafeteria from './views/Cafeteria';
+import Profile from './views/Profile';
+
 
 Vue.use(Router)
 
@@ -16,10 +20,17 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: '/cafeteria',
+      name: 'cafeteria',
+      component: Cafeteria
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
