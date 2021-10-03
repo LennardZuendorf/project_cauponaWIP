df4609b7ea54bb8953e3a5d9bb3b53351be705dd
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-03 15:01:33 +0200

    c1.3 - updated TopToolbar component

--- src/components/TopToolbar.vue
+++ src/components/TopToolbar.vue
@@ -1,59 +1,39 @@
 <template>
-  <v-card
-      class="mx-auto overflow-hidden"
-      height="400"
-  >
-    <v-app-bar
-        color="grey"
-        dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+  <v-card class="overflow-hidden">
+    <v-app-bar dense fixed color="background">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>CAUPONA</v-toolbar-title>
+      <v-row class="d-flex align-content-center justify-center">
+        <v-col cols="6" >
+          <v-toolbar-title >
+            <v-img :src="require('@/assets/caupona_logo_text.svg')" max-height="75px"></v-img>
+          </v-toolbar-title>
+        </v-col>
+      </v-row>
     </v-app-bar>
 
-    <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-    >
-      <v-list
-          nav
-          dense
-      >
-        <v-list-item-group
-            v-model="group"
-            active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Search</v-list-item-title>
-          </v-list-item>
+    <v-card-text>
+      <v-row>
+        Test1
+      </v-row>
+      <v-row>
+        Test2
+      </v-row>
+    </v-card-text>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Help</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+
   </v-card>
 </template>
 
 <script>
 export default {
-  name: "TopToolbar",
+  name: 'TopToolbar',
   data: () => ({
-    drawer: false,
-    group: null,
+    collapseOnScroll: true,
   }),
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
