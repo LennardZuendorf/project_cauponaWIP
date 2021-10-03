db921f135d08d4f398c5059f2ea3fa186de95748
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-03 23:13:22 +0200

    c3.4 - added about dialog

---
+++ src/components/Popups/About.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row justify="space-around">
+    <v-col cols="auto">
+
+      <v-dialog
+          transition="dialog-top-transition"
+          max-width="600"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+          >ABOUT</v-btn>
+        </template>
+
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar
+                color="primary"
+                dark
+            >ABOUT CAUPONA</v-toolbar>
+            <v-card-text>
+              <v-row>
+
+              </v-row>
+
+            </v-card-text>
+
+            <v-card-actions class="justify-end">
+              <v-btn
+                  text
+                  @click="dialog.value = false"
+              >close</v-btn>
+            </v-card-actions>
+          </v-card>
+
+        </template>
+      </v-dialog>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
