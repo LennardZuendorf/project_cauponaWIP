2091eba33e7d307548a2be540623083e52fb114c
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-04 20:37:10 +0100

    c3.9 - restructured Home.vue, added TODOs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,35 @@
 <template>
   <v-app>
-    <TopToolbar></TopToolbar>
     <v-main>
+      <v-container>
 
+        <v-row>
+          <v-col cols="10">
+            <div>FIND CANTINES</div> <!-- TODO: set Font Size and style-->
+            <map-box></map-box>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="10">
+            <div>NEAREST CANTINE:</div> <!-- TODO: set Font Size and style-->
+            <!-- TODO: add message boxes-->
+            <food-row></food-row>
+          </v-col>
+        </v-row>
+
+      </v-container>
     </v-main>
-    <BottomNav></BottomNav>
   </v-app>
 </template>
 
 <script>
-import TopToolbar from "@/components/TopToolbar";
-import BottomNav from "@/components/BottomNav";
+import MapBox from "@/components/Map/MapBox";
+import FoodRow from "@/components/Food/FoodRow";
 
 export default {
   name: "Home",
-  components: {BottomNav, TopToolbar}
+  components: {FoodRow, MapBox}
 }
 </script>
 
