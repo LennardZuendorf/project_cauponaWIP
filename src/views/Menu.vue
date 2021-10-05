e05d8a0fce8dbc6de2833755e2bd8ae49425bb99
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-06 00:12:38 +0100

    c4.17 - setup card for content box

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="Menu">
-    <h1>This is the Menu Page</h1>
+    <v-card
+        class="mx-auto"
+        max-width="95%"
+        max-height="95%"
+        elevation="3"
+        color="background"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mb-1">
+            Check out the menu
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Your selected cafeteria:
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-text>
+        <FoodBox></FoodBox>
+      </v-card-text>
+
+    </v-card>
   </div>
 </template>
 
 <script>
+import FoodBox from "@/components/Menu/FoodBox";
+
 export default {
   name: "Menu"
 }
