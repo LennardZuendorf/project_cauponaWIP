ce779efb39f37c78573c78277d60e9927e21e93a
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-06 00:12:51 +0100

    c4.18 - setup card for content box

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="Map">
-    <h1>This is the map Page</h1>
+    <v-card
+        class="mx-auto"
+        max-width="95%"
+        max-height="95%"
+        elevation="3"
+        color="background"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mb-1">
+            Check for the nearest cafeterias:
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-text>
+        <MapBox></MapBox>
+      </v-card-text>
+
+    </v-card>
   </div>
 </template>
 
 <script>
+import MapBox from "@/components/Map/MapBox";
 export default {
-  name: "Map"
+  name: "Map",
+  components: {MapBox}
 }
 </script>
 
