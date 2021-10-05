b9d280edb77490447b8e7568cbd2598e71e69b6e
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-05 23:37:45 +0100

    c4.8 - added, Settings.vue

---
+++ src/components/dialogs/Settings.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+  >
+  <template v-slot:activator="{ on, attrs }">
+    <v-btn
+        color="white"
+        dark
+        v-bind="attrs"
+        v-on="on"
+        text
+        block
+        large
+    >
+      SETTINGS
+    </v-btn>
+  </template>
+  <v-card>
+    <v-toolbar
+        dark
+        color="primary"
+    >
+      <v-btn
+          icon
+          dark
+          @click="dialog = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>ABOUT</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn
+            dark
+            text
+            @click="dialog = false"
+        >
+          SETTINGS
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false,
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
