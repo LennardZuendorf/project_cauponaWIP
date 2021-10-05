377bfbbfba8c750967a145e0920e2fd881ec02d2
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-05 21:59:54 +0100

    c4.6 - last badge of structural updates for version 2.ß

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
+    theme: { dark: false,
         options: {
             customProperties: true,
         },
@@ -18,20 +18,20 @@ export default new Vuetify({
                 success: '#558B2F',
                 warning: '#F9A825',
                 background: '#FAFAFA',
-                background2: '#E0E0E0',
-                grey: '#616161'
-            },
-            dark: {
-                primary: '#558B2F',
-                secondary: '#0277BD',
-                accent: '#F9A825',
-                error: '#FF8F00',
-                info: '#29B6F6',
-                success: '#7CB342',
-                warning: '#FFEE58'',
-                background: '#424242',
                 background2: '#616161',
                 grey: '#E0E0E0'
+            },
+            dark: {
+                primary: '#7CB342',
+                secondary: '#29B6F6',
+                accent: '#FFEE58',
+                error: '#FF8F00',
+                info: '#0277BD',
+                success: '#558B2F',
+                warning: '#F9A825',
+                background: '#424242',
+                background2: '#616161',
+                grey: '#E0E0E0',
             }
         },
     }
