c8f36a85f8c8cac7ce5cf69b159330295cbfeab2
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-06 21:57:39 +0100

    c5.1 - fixed dialogs, added register

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,40 +1,48 @@
 <template>
     <span>
         <v-navigation-drawer app v-model="drawer" color="primary" dark disable-resize-watcher>
-          <v-container align-content="end" fill-height>
+          <v-container align-content="start" full-height>
 
             <v-row>
-              <v-spacer></v-spacer>
-              <About></About>
-              <v-spacer></v-spacer>
-            </v-row>
 
-            <v-row>
-              <v-spacer></v-spacer>
-              <Login></Login>
-              <v-spacer></v-spacer>
-            </v-row>
+              <v-col cols="12">
+                <v-spacer></v-spacer>
+                <About></About>
+                <v-spacer></v-spacer>
+              </v-col>
 
-            <v-row>
-              <v-spacer></v-spacer>
-                  <v-btn
+              <v-col cols="12">
+                <v-spacer></v-spacer>
+                <Login></Login>
+                <v-spacer></v-spacer>
+              </v-col>
+
+              <v-col cols="12">
+                <v-spacer></v-spacer>
+                <Register></Register>
+                <v-spacer></v-spacer>
+              </v-col>
+
+              <v-col cols="12">
+                <v-spacer></v-spacer>
+                <v-btn
                       color="white"
                       dark
                       text
                       block
                       large
-                  >
+                >
                     LOGOUT
-                  </v-btn>
-              <v-spacer></v-spacer>
-            </v-row>
+                </v-btn>
+                <v-spacer></v-spacer>
+              </v-col>
+              <v-col cols="12">
+                <v-spacer></v-spacer>
+                <Settings></Settings>
+                <v-spacer></v-spacer>
+              </v-col>
 
-            <v-row>
-              <v-spacer></v-spacer>
-              <Settings></Settings>
-              <v-spacer></v-spacer>
             </v-row>
-
           </v-container>
 
         </v-navigation-drawer>
@@ -43,7 +51,7 @@
           <v-spacer></v-spacer>
 
           <v-toolbar-title>
-            <h3>CAUPONA</h3>
+            <v-img :src="require('@/assets/caupona_logo_long_white.svg')" max-height="75px"></v-img>
           </v-toolbar-title>
           <v-spacer></v-spacer>
         </v-app-bar>
@@ -54,10 +62,11 @@
 import About from "@/components/dialogs/About";
 import Login from "@/components/dialogs/Login";
 import Settings from "@/components/dialogs/Settings";
+import Register from "@/components/dialogs/Register";
 
 export default {
   name: 'Toolbar',
-  components: {Login, About, Settings},
+  components: {Register, Login, About, Settings},
 
   data() {
     return {
