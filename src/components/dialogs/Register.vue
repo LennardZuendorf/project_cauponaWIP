7dbf7223581bc68ff73f79b2056db76709de6522
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-07 01:06:08 +0100

    c5.12 - implemented register features

--- src/components/dialogs/Register.vue
+++ src/components/dialogs/Register.vue
@@ -38,30 +38,6 @@
         <v-container>
           <v-row align-content="center">
 
-            <v-col
-                cols="12"
-                md="4"
-            >
-              <v-text-field
-                  v-model="firstname"
-                  :rules="nameRules"
-                  label="first name"
-                  required
-              ></v-text-field>
-            </v-col>
-
-            <v-col
-                cols="12"
-                md="4"
-            >
-              <v-text-field
-                  v-model="lastname"
-                  :rules="nameRules"
-                  label="last name"
-                  required
-              ></v-text-field>
-            </v-col>
-
             <v-col cols="12">
               <v-text-field
                   v-model="email"
@@ -114,23 +90,41 @@
 
 <script>
 export default {
-  name: "Register",
+  name:"Register",
   data() {
     return {
       dialog: false,
       notifications: false,
       sound: true,
       widgets: false,
-      show1: false,
-      show2: true,
-      show3: false,
-      show4: false,
-      password: 'Password',
-      rules: {
-        required: value => !!value || 'Required.',
-        min: v => v.length >= 8 || 'Min 8 characters',
-        emailMatch: () => (`The email and password you entered don't match`),
-      },
+      show: false,
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v =>
+            v.length >= 6 ||
+            'Password must be greater than 6 characters'
+      ],
+      methods: {
+        submit() {
+          if (this.$refs.form.validate()) {
+            this.$store.dispatch('userRegister', {
+              email: this.email,
+              password: this.password
+            });
+          }
+        },
+        clear(){
+          this.password = "";
+          this.email = "";
+        }
+      }
     }
   },
 }
