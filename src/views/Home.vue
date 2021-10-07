02efa588fca7500e5755ba37d85fa33effd3f726
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-07 01:04:43 +0100

    c5.8 - updated home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,53 +2,60 @@
   <div class="Home">
     <v-container fill-height>
 
-    <v-card
-        class="mx-auto"
-        width="95%"
-        height="45%"
-        elevation="3"
-        color="background"
-    >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h5 mb-1">
-            Check for the nearest cafeterias:
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-row>
+        <v-card
+            class="mx-auto"
+            width="95%"
+            height="45%"
+            elevation="3"
+            color="background"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-1">
+                find nearby cantines
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-      <v-card-text>
-        <MapBox></MapBox>
-      </v-card-text>
-    </v-card>
+          <v-card-text>
+            <MapBox></MapBox>
+          </v-card-text>
 
-    <v-card
-        class="mx-auto"
-        width="95%"
-        height="45%"
-        elevation="3"
-        color="background"
-    >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h5 mb-1">
-            Check the food at the nearest Cafeteria
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Your nearest Cafeteria:
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-card-text>
-      </v-card-text>
-    </v-card>
+        </v-card>
+      </v-row>
+
+      <v-row>
+        <v-card
+          class="mx-auto"
+          width="95%"
+          height="45%"
+          elevation="3"
+          color="background"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-1">
+                discover the nearest cantines
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Cantine XY
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-card-text>
+            <foo
+          </v-card-text>
+      </v-card>
+
+      </v-row>
     </v-container>
 
   </div>
 </template>
 
 <script>
-import MapBox from "@/components/Map/MapBox";
+import MapBox from "@/components/MensaOverview/MensaList";
 export default {
   name: "Home",
   components: {MapBox}
