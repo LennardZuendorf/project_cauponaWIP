17a954cf613f32404ad5bb5131ae75406e7ac025
Goekalp <Goekalpyildiz@outlook.de>
2021-10-07 17:01:40 +0200

    c4.24 edit Menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="Menu">
-    <v-container fill-height>
 
+    <v-container fill-height>
+      <h1>Check out the Menü</h1>
+   <!--
     <v-card
         class="mx-auto"
         width="95%"
@@ -25,7 +27,84 @@
       </v-card-text>
 
     </v-card>
+    -->
     </v-container>
+
+    <v-container fill-height>
+      <h1>TODAYS SPECIALS</h1>
+      <div class="cardContainer">
+      <v-card v-for="today in anzTodays" :key="today"
+          class="mx-auto card"
+        >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h5 mb-1">
+             Todays Special
+            </v-list-item-title>
+            <v-list-item-subtitle>
+
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-card-text>
+          <FoodBox></FoodBox>
+        </v-card-text>
+
+      </v-card>
+      </div>
+    </v-container>
+
+    <v-container fill-height>
+      <h1>Main Courses</h1>
+      <div class="cardContainer">
+        <v-card v-for="menu in anzMenues" :key="menu"
+                class="mx-auto card"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-1">
+                Main Courses
+              </v-list-item-title>
+              <v-list-item-subtitle>
+
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-text>
+            <FoodBox></FoodBox>
+          </v-card-text>
+
+        </v-card>
+      </div>
+    </v-container>
+
+    <v-container fill-height>
+      <h1>DESSERTS</h1>
+      <div class="cardContainer">
+        <v-card v-for="dessert in anzDesserts" :key="dessert"
+                class="mx-auto card"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-1">
+                Dessert
+              </v-list-item-title>
+              <v-list-item-subtitle>
+
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-text>
+            <FoodBox></FoodBox>
+          </v-card-text>
+
+        </v-card>
+      </div>
+    </v-container>
+
   </div>
 </template>
 
@@ -33,10 +112,26 @@
 import FoodBox from "@/components/Menu/FoodBox";
 
 export default {
-  name: "Menu"
+  name: "Menu",
+
+  data(){
+    return{anzMenues: 5,
+      anzTodays: 5,
+      anzDesserts: 5}
+  }
 }
 </script>
 
 <style scoped>
-
+.card{
+  width: 170px;
+  height: 170px;
+  elevation: 3;
+  color: background;
+}
+.cardContainer{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
 </style>
