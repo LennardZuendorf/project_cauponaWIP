3f52bc214f3aac0afb48a3efdfb5fbd8823ac400
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-10-07 01:05:28 +0100

    c5.10 - added firebase to main

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from "./firebase";
 
 Vue.config.productionTip = false
 
