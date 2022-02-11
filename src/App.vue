48bb229162e4d40a48d0fe58b63ba9bcd6df5bf1
LennardZuendorf <l.p.zuendorf@gmail.com>
2022-02-11 16:59:08 +0100

    c8.3 reset 3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
 <style>
 #app {
-  font-family: Roboto, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,18 @@
 <template>
-  <div class="Home">
-    <v-container full-height>
-
-      <v-col>
-        <v-card
-            height="40%"
-            elevation="3"
-            color="background"
-        >
-
-          <v-card-title>
-            see the nearest cantines
-          </v-card-title>
-
-          <v-card-subtitle>
-            Please allows us to access your location!
-          </v-card-subtitle>
-
-          <MensaListMini></MensaListMini>
-
-        </v-card>
-      </v-col>
-
-      <v-spacer></v-spacer>
-
-      <v-col>
-        <v-card
-            height="40%"
-            elevation="3"
-            color="background"
-        >
-
-          <v-card-title>
-            discover one cantine
-          </v-card-title>
-
-          <v-card-subtitle>
-            {{selectedCafeteria.name}}
-          </v-card-subtitle>
-
-          <v-card-text>
-            <FoodBoxMini></FoodBoxMini>
-          </v-card-text>
-
-        </v-card>
-      </v-col>
-
-    </v-container>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
-import FoodBoxMini from "@/components/Menu/FoodBoxMini";
-import MensaListMini from "@/components/MensaOverview/MensaListMini";
-
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
-  components: {FoodBoxMini, MensaListMini},
-
-  computed: mapState([
-    'closeCantines', 'selectedCafeteria'
-  ]),
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
 }
 </script>
-
-<style>
-
-</style>
